32f132377a9f4ee8f0a2d646de3f573e5d871d46
Anatoliy Sova <sovaar@hotmail.com>
2026-08-17 00:14:14 +0300

    Monastery basement archives

--- Source/area.cpp
+++ Source/area.cpp
@@ -1144,6 +1144,7 @@ static const char *TintedAreas[] =
 {
 	"jerrockmines",
 	"darkpath",
+	"archives",		//the Monastery basement
 };
 
 //height inside a terrain quad.  u/v run 0..1 from the quad's NW corner.
